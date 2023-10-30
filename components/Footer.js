cba5e21e03d9749395a4fc9a5d11b43e351421ee
test <Workoutwwill@gmail.com>
2023-10-30 15:26:19 -0400

    changed footer text

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <footer className="bg-white">
             <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
              
-                <p className="mt-8 text-center text-gray-400">&copy; 2021 Simple Skin, All rights reserved.</p>
+                <p className="mt-8 text-center text-gray-400">&copy; 2021 SentimBox, All rights reserved.</p>
             </div>
             
         </footer>
