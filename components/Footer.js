7cf87b5d74518d4cdd65d1383d632076eda81aa8
test <Workoutwwill@gmail.com>
2023-10-31 06:35:11 -0400

    nextlink

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const navigation = [
     {name: 'About', href: '#'},
     {name: 'Careers', href: '#'},
@@ -16,17 +18,24 @@ const Footer = () => {
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Sentimbox</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                        <li>
-                            <a href="/about" className="mr-4 hover:underline md:mr-6 ">About</a>
-                        </li>
-                        
-                        <li>
-                            <a href="/refundpolicy" className="mr-4 hover:underline md:mr-6 ">Refund Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
-                        </li>
-                    </ul>
+      <li>
+        <Link href="/about">
+          <a className="mr-4 hover:underline md:mr-6 ">About</a>
+        </Link>
+      </li>
+
+      <li>
+        <Link href="/refundpolicy">
+          <a className="mr-4 hover:underline md:mr-6 ">Refund Policy</a>
+        </Link>
+      </li>
+
+      <li>
+        <Link href="#">
+          <a className="hover:underline">Contact</a>
+        </Link>
+      </li>
+    </ul>
                 </div>
                 <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                 <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a  className="hover:underline">SentimBox™</a>. All Rights Reserved.</span>
